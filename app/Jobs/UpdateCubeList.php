a2de14a9e22bee52ffd139ce0ada373bbef6aa5c
“Teddy <teddyrlee@gmail.com>
2022-12-14 14:31:39 -0800

    debugging

--- app/Jobs/UpdateCubeList.php
+++ app/Jobs/UpdateCubeList.php
@@ -3,6 +3,7 @@
 namespace App\Jobs;
 
 use App\Models\Card;
+use App\Models\CardFace;
 use App\Models\CubeList;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
@@ -34,7 +35,8 @@ class UpdateCubeList implements ShouldQueue
     {
         $handle = fopen("storage/app/cube_list.csv", "r");
         if ($handle) {
-            while (($line = fgetcsv($handle)) !== false) {
+            while (($line = fgets($handle)) !== false) {
+                $line = explode(',', $line);
                 echo $line[1] . PHP_EOL;
                 echo $line[0] . PHP_EOL;
                 echo $line[2] . PHP_EOL;
@@ -42,7 +44,7 @@ class UpdateCubeList implements ShouldQueue
                 CubeList::updateOrCreate(
                     ['sleeve_id' => $line[1]],
                     [
-                        'oracle_id' => Card::firstWhere('name', 'like',  $line[0] . '%')->oracle_id,
+                        'oracle_id' => CardFace::firstWhere('name', $line[0])->oracle_id,
                         'layout_key_1' => $line[2],
                         'layout_key_2' => $line[3]
                     ]
