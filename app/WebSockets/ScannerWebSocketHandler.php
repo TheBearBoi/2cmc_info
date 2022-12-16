a29d9f67cc4ec6a55a0c640293b82f20df3cf113
“Teddy <teddyrlee@gmail.com>
2022-12-15 22:42:34 -0800

    websocket

--- app/WebSockets/ScannerWebSocketHandler.php
+++ app/WebSockets/ScannerWebSocketHandler.php
@@ -18,7 +18,6 @@ class ScannerWebSocketHandler implements MessageComponentInterface
     public function onOpen(ConnectionInterface $conn) {
         // Store the new connection to send messages to later
         $this->clients->attach($conn);
-        $conn->send('connection succesful!');
 
         echo "New connection! ({$conn->resourceId})\n";
     }
