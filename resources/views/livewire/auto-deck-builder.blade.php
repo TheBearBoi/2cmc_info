849b47a85e6f367efa6eef2d914691a5a95f003e
“Teddy <teddyrlee@gmail.com>
2022-12-23 19:47:52 -0600

    more work on getting private channels to work

--- resources/views/livewire/auto-deck-builder.blade.php
+++ resources/views/livewire/auto-deck-builder.blade.php
@@ -1,7 +1,7 @@
 <div x-data="{ open: @entangle('show') }">
     <button wire:click="toggle_scanner" x-show="!open">Automatic</button>
-    <div  x-show="open" x-cloak class="absolute inset-4 rounded border-black border-8 bg-slate-300">
-        <img src="{{ $this->most_recent_card->faces->first()->large_image_uri }}" alt="{{ $this->most_recent_card->name }}"/>
+    <div  x-show="open" x-cloak class="absolute inset-4 rounded-lg border-black border-8 bg-slate-300">
+        <img src="{{ $this->most_recent_card->faces->first()->large_image_uri }}" alt="{{ $this->most_recent_card->name }}" class="h-full"/>
         <ul>
             @foreach($this->deck_card_names as $card_name)
                 <li>{{ $card_name }}</li>
