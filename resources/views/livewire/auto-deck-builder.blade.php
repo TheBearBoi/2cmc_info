3f037857a0c66ad6e8636f2c9507f2656b0862f6
“Teddy <teddyrlee@gmail.com>
2022-12-26 17:53:06 -0600

    mostly css and smaller functions

--- resources/views/livewire/auto-deck-builder.blade.php
+++ resources/views/livewire/auto-deck-builder.blade.php
@@ -1,22 +1,22 @@
 <div x-data="{ open: @entangle('show') }">
     <button wire:click="toggle_scanner" x-show="!open">Automatic</button>
     <div x-show="open" x-cloak class="absolute inset-0 bg-opacity-80 bg-black">
-        <div>
-            <label for="name{{ $seat->seat_id }}">Deck Name:</label><input id="name{{ $seat->seat_id }}" name="name{{ $seat->seat_id }}" type="text" wire:model.defer="deck_name"><br />
-            <label for="archetype{{ $seat->seat_id }}">Archetype:</label><input id="archetype{{ $seat->seat_id }}" name="archetype{{ $seat->seat_id }}" type="text" wire:model.defer="archetype"><br />
-            <label for="color{{ $seat->seat_id }}">Color:</label><br />
-            <input type="checkbox" id="white{{ $seat->seat_id }}" name="white{{ $seat->seat_id }}" value="W" wire:model.defer="colors.W">
-            <label for="white{{ $seat->seat_id }}">W</label><br>
-            <input type="checkbox" id="blue{{ $seat->seat_id }}" name="blue{{ $seat->seat_id }}" value="U" wire:model.defer="colors.U">
-            <label for="blue{{ $seat->seat_id }}">U</label><br>
-            <input type="checkbox" id="black{{ $seat->seat_id }}" name="black{{ $seat->seat_id }}" value="B" wire:model.defer="colors.B">
-            <label for="black{{ $seat->seat_id }}">B</label><br>
-            <input type="checkbox" id="red{{ $seat->seat_id }}" name="red{{ $seat->seat_id }}" value="R" wire:model.defer="colors.R">
-            <label for="red{{ $seat->seat_id }}">R</label><br>
-            <input type="checkbox" id="green{{ $seat->seat_id }}" name="green{{ $seat->seat_id }}" value="G" wire:model.defer="colors.G">
-            <label for="green{{ $seat->seat_id }}">G</label><br>
-        </div>
         <div  class="absolute inset-8 rounded-lg border-black border-8 bg-slate-300 p-4 flex">
+            <div>
+                <label for="name{{ $seat->seat_id }}">Deck Name:</label><input id="name{{ $seat->seat_id }}" name="name{{ $seat->seat_id }}" type="text" wire:model.defer="deck_name"><br />
+                <label for="archetype{{ $seat->seat_id }}">Archetype:</label><input id="archetype{{ $seat->seat_id }}" name="archetype{{ $seat->seat_id }}" type="text" wire:model.defer="archetype"><br />
+                <label for="color{{ $seat->seat_id }}">Color:</label><br />
+                <input type="checkbox" id="white{{ $seat->seat_id }}" name="white{{ $seat->seat_id }}" value="W" wire:model.defer="colors.W">
+                <label for="white{{ $seat->seat_id }}">W</label><br>
+                <input type="checkbox" id="blue{{ $seat->seat_id }}" name="blue{{ $seat->seat_id }}" value="U" wire:model.defer="colors.U">
+                <label for="blue{{ $seat->seat_id }}">U</label><br>
+                <input type="checkbox" id="black{{ $seat->seat_id }}" name="black{{ $seat->seat_id }}" value="B" wire:model.defer="colors.B">
+                <label for="black{{ $seat->seat_id }}">B</label><br>
+                <input type="checkbox" id="red{{ $seat->seat_id }}" name="red{{ $seat->seat_id }}" value="R" wire:model.defer="colors.R">
+                <label for="red{{ $seat->seat_id }}">R</label><br>
+                <input type="checkbox" id="green{{ $seat->seat_id }}" name="green{{ $seat->seat_id }}" value="G" wire:model.defer="colors.G">
+                <label for="green{{ $seat->seat_id }}">G</label><br>
+            </div>
             <img src="{{ $this->most_recent_card->faces->first()->large_image_uri }}" alt="{{ $this->most_recent_card->name }}" class="h-full"/>
             <ul>
                 <li class="text-lg">Main Deck</li>
