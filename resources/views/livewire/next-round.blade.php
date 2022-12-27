d8af590c3a62f481c7aceb5fd94b1bce52337f1c
“Teddy <teddyrlee@gmail.com>
2022-12-27 15:30:18 -0600

    mostly css and smaller functions

--- resources/views/livewire/next-round.blade.php
+++ resources/views/livewire/next-round.blade.php
@@ -1,3 +1,8 @@
 <button wire:click="next_round">
     {{ $label }}
 </button>
+<script>
+    window.addEventListener('contentChanged', (t) => {
+        alert('Please submit all ' + t + ' before moving to the next round!');
+    });
+</script>
