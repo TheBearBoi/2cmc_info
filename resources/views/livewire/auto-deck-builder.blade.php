84c5f31d99b1f5b21a0c8f4e8ba8f32889024108
“Teddy <teddyrlee@gmail.com>
2022-12-26 20:17:03 -0600

    mostly css and smaller functions

--- resources/views/livewire/auto-deck-builder.blade.php
+++ resources/views/livewire/auto-deck-builder.blade.php
@@ -25,7 +25,6 @@
             @else
                 <img src="{{ $most_recent_card->faces->first()->png_uri }}" alt="{{ $this->most_recent_card->name }}" class="h-full"/>
             @endif
-            <img src="{{ isset($most_recent_card) ? $most_recent_card->faces->first()->png_uri : 'https://static.wikia.nocookie.net/mtgsalvation_gamepedia/images/f/f8/Magic_card_back.jpg/revision/latest?cb=20140813141013' }}" alt="{{ $this->most_recent_card->name }}" class="h-full"/>
             <ul class="m-4 grow">
                 <li class="text-lg">Main Deck</li>
                 @foreach($this->main_deck_list as $value)
