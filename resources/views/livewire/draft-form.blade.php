39c80feba97a98cc0209f67a6fb6e147a65527b7
“Teddy <teddyrlee@gmail.com>
2022-12-27 21:10:28 -0600

    mostly css and smaller functions

--- resources/views/livewire/draft-form.blade.php
+++ resources/views/livewire/draft-form.blade.php
@@ -1,20 +1,19 @@
 <form wire:submit.prevent="createDraft">
     @csrf
-    <label for="team">Team Draft:</label><input type="checkbox" id="team" name="team" wire:model="is_team_draft"><br />
-    <label for="round-time">Round Timer (mins):</label> <input type="text" id="round-time" wire:model="round_time"><br />
+{{--  Todo Move team draft and round timer to bottom of the page!  --}}
     <table class="mx-auto">
         <tr>
-            <th class="mx-2">Player</th>
-            <th class="mx-2">Assigned Seat</th>
-            <th class="mx-2">Remove Player</th>
+            <th class="mx-16">Player</th>
+            <th class="mx-16">Assigned Seat</th>
+            <th class="mx-16">Remove Player</th>
         </tr>
 
-        @foreach($draft_seats as $key => $draft_seat)
+    @foreach($draft_seats as $key => $draft_seat)
             <tr>
                 <td>{{ $draft_seat["player"]["player_name"]}}</td>
                 <td><select wire:model="draft_seats.{{ $key }}.seat_number">
                         <option value=""></option>
-                        @foreach(range(1,$player_count, 1) as $i)
+                    @foreach(range(1,$player_count, 1) as $i)
 
                             <option value="{{ $i }}">{{ $i }}</option>
                         @endforeach
@@ -24,10 +23,10 @@
         @endforeach
     </table>
     <div class="absolute bottom-24 left-0 right-64 w-80 mx-auto text-center">
-        <div class="mx-auto">
-            <input class="bg-slate-300" type="text" list="previous-players" value="" wire:model="new_player_name">
-            <button wire:click.prevent="addPlayer()">Add Player</button>
-        </div>
+        <label for="team">Team Draft:</label><input type="checkbox" id="team" name="team" wire:model="is_team_draft"><br />
+        <label for="round-time">Round Timer (mins):</label> <input type="text" id="round-time" wire:model="round_time"><br />
+        <button wire:click.prevent="addPlayer()">Add Player</button>
+        <input class="bg-slate-300" type="text" list="previous-players" value="" wire:model="new_player_name"><br />
         <datalist id="previous-players">
             @foreach($remaining_previous_players as $previous_player)
                 <option value="{{ $previous_player->player_name }}"></option>
